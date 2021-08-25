03c7f865a1dd34e0d857e1e275a7330af6fbda3d
Alice Williams <alice@10degrees.uk>
2021-08-25 19:25:45 +0100

    Add load more button for playlists.

--- src/components/PlaylistSongs.js
+++ src/components/PlaylistSongs.js
@@ -6,26 +6,41 @@ import Song from './Song';
 const PlaylistSongs = ({playlistID}) => {
 	let spotify = useContext(SpotifyContext);
 	let [songs, setSongs] = useState([]);
+	let [nextLink, setNextLink] = useState('');
 
 	useEffect(() => {
 		spotify.getSongsForPlaylist(playlistID)
 			.then(songs => {
-				setSongs(songs.items)
+				setNextLink(songs.next);
+				setSongs(songs.items);
 			});
-	}, [setSongs, spotify, playlistID])
+	}, [setSongs, spotify, playlistID, setNextLink]);
+
+	const loadMore = () => {
+		spotify.get(nextLink)
+			.then(res => res.json())
+			.then((songs) => {
+				setNextLink(songs.next)
+				setSongs(prevSongs => prevSongs.concat(songs.items))
+			});
+	}
 
 	if(!songs) {
 		return <p>Loading...</p>
 	}
 
 	return (
-		<ul className="space-y-2">
-			{songs.map((song, idx) => (
-				<li key={song.track.id}>
-					<Song song={song} index={idx + 1}/>
-				</li>
-			))}
-		</ul>
+		<div>
+			<ul className="space-y-2">
+				{songs.map((song, idx) => (
+					<li key={song.track.id}>
+						<Song song={song} index={idx + 1}/>
+					</li>
+				))}
+			</ul>
+
+			{nextLink ? <div className="flex justify-center"><button onClick={loadMore} className="border-2 text-white border-white rounded-full mt-6 mx-auto p-4">Load More</button></div> : null}
+		</div>
 	)
 }
 export default PlaylistSongs;
--- src/components/Song.js
+++ src/components/Song.js
@@ -14,14 +14,14 @@ const Song = ({song, index}) => {
 	return (
 		<div className="flex items-center w-full">
 			<span className="w-8 text-right mr-6">{index}</span>
-			<span className="w-1/3 flex items-center mr-8 flex-1">
+			<span className="w-1/3 flex items-center mr-8">
 				{albumSrc ? <img src={albumSrc} alt="" className="h-12 w-12 mr-4"/> : null}
 				<div className="overflow-ellipsis whitespace-nowrap overflow-hidden">
 					<p className='overflow-ellipsis whitespace-nowrap overflow-hidden'>{song.track.name}</p>
 					<p className="text-grey-300 text-sm overflow-ellipsis whitespace-nowrap overflow-hidden">{artists}</p>
 				</div>
 			</span>
-			<span className="text-grey-300 text-sm w-1/5 whitespace-nowrap overflow-hidden overflow-ellipsis text-sm mr-8 flex-1">{song.track.album.name}</span>
+			<span className="text-grey-300 text-sm w-1/5 whitespace-nowrap overflow-hidden overflow-ellipsis text-sm mr-8">{song.track.album.name}</span>
 			<span className="w-1/5 text-sm text-grey-300 mr-8">{dayjs(song.added_at).format('MMM D, YYYY')}</span>
 			<span className="text-sm text-grey-300">{millisToMinutesAndSeconds(song.track.duration_ms)}</span>
 		</div>
