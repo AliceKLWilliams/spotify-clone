6bb7273029e4928e0e5d483cba81c05802132c9b
Alice Williams <alice@10degrees.uk>
2021-10-01 16:50:09 +0100

    Create page for data-related components.

---
+++ src/components/pages/WhatYouLove.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WhatYouLove = () => {
+    return (
+        <div>
+            <h1 className="font-black text-5xl">What you love</h1>
+        </div>
+    )
+}
+
+export default WhatYouLove;
