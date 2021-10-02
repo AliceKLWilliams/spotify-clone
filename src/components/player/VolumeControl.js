de2b45b5537926618803f41f32a2ac573774e107
Alice Williams <alice@10degrees.uk>
2021-10-02 15:11:10 +0100

    Get the user's volume control on first load.

--- src/components/player/VolumeControl.js
+++ src/components/player/VolumeControl.js
@@ -1,17 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import SpotifyContext from '../../contexts/SpotifyContext';
 
 const VolumeControl = () => {
     let spotify = useContext(SpotifyContext);
+    let [volume, setVolume] = useState(0);
 
     const changeVolume = (e) => {
         let value = e.target.value;
 
-        spotify.changeVolume(value);
+        spotify.changeVolume(value)
+            .then(setVolume(value));
     }
 
+    useEffect(() => {
+        spotify.getPlaybackState()
+            .then(playbackState => {
+                setVolume(playbackState.device.volume_percent)
+            })
+    }, [setVolume, spotify])
+
     return (
-        <input className="w-32" min="0" max="100" step="1" type="range" onChange={changeVolume}/>
+        <input value={volume} className="w-32" min="0" max="100" step="1" type="range" onChange={changeVolume}/>
     )
 }
 
